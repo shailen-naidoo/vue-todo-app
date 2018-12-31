d9e97e44d62453ce3d87781abab8a074ef1007f9
Shailen <shailen@gettruck.co.za>
2018-12-31 10:36:23 +0200

    moved ui state to mixin

---
+++ src/mixins/ui-state.js
@@ -0,0 +1,9 @@
+export default {
+  data() {
+    return {
+      colors: ["red","blue","blue lighten-2","purple","yellow","orange","green","grey"],
+      mini: true,
+      show_create_todo: false
+    }
+  }
+}
